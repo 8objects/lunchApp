301a61586b0a99f72b147051a5890aa5e339b0db
Adam Brandt <adamusb@gmail.com>
2014-06-17 18:15:45 +0200

    angular app

---
+++ lunch-web/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('lunchWebApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
