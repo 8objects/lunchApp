46b3b5c46877b32f95e642d741b74fbeb798c066
Adam Brandt <adamusb@gmail.com>
2014-08-11 15:26:20 +0200

    angular app

---
+++ lunch-rs/src/main/java/com/eightobjects/lunches/rs/dto/ContactDto.java
@@ -0,0 +1,44 @@
+package com.eightobjects.lunches.rs.dto;
+
+/**
+ * Created by Adam on 2014-07-25.
+ */
+public class ContactDto extends LinkDto {
+    private String name;
+    private String email;
+    private String id;
+    private String photoLink;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhotoLink() {
+        return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+}
---
+++ lunch-rs/src/main/java/com/eightobjects/lunches/rs/dto/LinkDto.java
@@ -0,0 +1,16 @@
+package com.eightobjects.lunches.rs.dto;
+
+/**
+ * Created by Adam on 2014-08-11.
+ */
+public class LinkDto {
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
